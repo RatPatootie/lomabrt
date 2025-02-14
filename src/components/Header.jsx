9ab795d60638ab580159126e310aaa5b44ea7d1e
Petro <129007162+RatPatootie@users.noreply.github.com>
2025-02-15 00:22:27 +0200

    localizate header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
       <img src={image}   alt="logo" />
       <nav>
         <ul className='flex space-x-4'>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#">Головна</a></li>
+          <li><a href="#about">Про нас</a></li>
+          <li><a href="#contact">Наші контакти</a></li>
         </ul>
       </nav>
     </header>
